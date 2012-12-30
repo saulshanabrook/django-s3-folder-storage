f5ab6b08930ae3741e88f3a9e11c0d0065df2c6a
Saul Shanabrook <s.shanabrook@gmail.com>
2012-12-30 17:53:53 -0500

    Added caching to S3 storages

--- s3_folder_storage/s3.py
+++ s3_folder_storage/s3.py
@@ -1,11 +1,25 @@
-"""
-    Two classes for media storage
-"""
-
 from storages.backends.s3boto import S3BotoStorage
 from django.conf import settings
 
-class StaticStorage(S3BotoStorage):
+
+class CachedS3BotoStorage(S3BotoStorage): 
+        """ 
+        django-compressor uses this class to gzip the compressed files and send them to s3 
+        these files are then saved locally, which ensures that they only create fresh copies 
+        when they need to 
+        """ 
+        def __init__(self, *args, **kwargs): 
+            super(CachedS3BotoStorage, self).__init__(*args, **kwargs) 
+            self.local_storage = get_storage_class('compressor.storage.CompressorFileStorage')() 
+
+
+        def save(self, filename, content): 
+            filename = super(CachedS3BotoStorage, self).save(filename, content) 
+            self.local_storage._save(filename, content) 
+            return filename 
+
+
+class StaticStorage(CachedS3BotoStorage):
     """
     Storage for static files.
     The folder is defined in settings.STATIC_S3_PATH
@@ -15,7 +29,8 @@ class StaticStorage(S3BotoStorage):
         kwargs['location'] = settings.STATIC_S3_PATH
         super(StaticStorage, self).__init__(*args, **kwargs)
 
-class DefaultStorage(S3BotoStorage):
+
+class DefaultStorage(CachedS3BotoStorage):
     """
     Storage for uploaded media files.
     The folder is defined in settings.DEFAULT_S3_PATH
